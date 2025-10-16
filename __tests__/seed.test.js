2d195e6d19012825ee30c197f24de8a9ea9ff516
Dan Moffat <danmoffat@gmail.com>
2025-10-16 15:10:51 +0100

    Added pg-format

--- __tests__/seed.test.js
+++ __tests__/seed.test.js
@@ -559,7 +559,7 @@ describe('seed', () => {
   });
 });
 
-describe('data insertion', () => {
+xdescribe('data insertion', () => {
   test('topics data has been inserted correctly', () => {
     return db.query(`SELECT * FROM topics;`).then(({ rows: topics }) => {
       expect(topics).toHaveLength(3);
