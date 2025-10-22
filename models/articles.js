d84754b3b47a644b51ea7398f6c995a423786f19
Dan Moffat <danmoffat@gmail.com>
2025-10-22 12:18:31 +0100

    Updated readallArticles sql query to include comment_count

--- models/articles.js
+++ models/articles.js
@@ -3,7 +3,12 @@ const db = require('../db/connection');
 exports.readAllArticles = async () => {
   try {
     const response = await db.query(
-      'SELECT article_id, title, topic, author, created_at, votes, article_img_url FROM articles ORDER BY created_at DESC'
+      `SELECT articles.article_id, articles.title, articles.topic, articles.author, articles.created_at, articles.votes, articles.article_img_url, COUNT(comments.article_id) AS comment_count
+      FROM articles
+      JOIN comments
+      ON articles.article_id = comments.article_id
+      GROUP BY articles.article_id
+      ORDER BY articles.created_at DESC;`
     );
     const topics = response.rows;
     return topics;
