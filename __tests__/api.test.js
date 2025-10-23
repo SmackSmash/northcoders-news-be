3a0851b9be5aee2264a23990478d5e73158eb2d0
Dan Moffat <danmoffat@gmail.com>
2025-10-23 14:18:20 +0100

    Consolidated tests to a single test file

--- __tests__/api.test.js
+++ __tests__/api.test.js
@@ -4,11 +4,35 @@ const seed = require('../db/seeds/seed');
 const data = require('../db/data/test-data');
 const app = require('../app');
 
-beforeAll(() => seed(data));
+beforeEach(() => seed(data));
 afterAll(() => db.end());
 
-describe('GET /', () => {
-  it('retreives an array of articles with the correct data', () => {
+// TOPICS
+describe('GET /api/topics', () => {
+  it('200: retrieves an array of topics with the correct data', () => {
+    return request(app)
+      .get('/api/topics')
+      .expect(200)
+      .then(res => {
+        const topics = res.body.topics;
+
+        expect(Array.isArray(topics)).toBe(true);
+        expect(topics.length).toBeGreaterThan(0);
+
+        topics.forEach(topic => {
+          const { slug, description, img_url } = topic;
+
+          expect(typeof slug).toBe('string');
+          expect(typeof description).toBe('string');
+          expect(typeof img_url).toBe('string');
+        });
+      });
+  });
+});
+
+// ARTICLES
+describe('GET /api/articles', () => {
+  it('200: retrieves an array of articles with the correct data', () => {
     return request(app)
       .get('/api/articles')
       .expect(200)
@@ -33,7 +57,7 @@ describe('GET /', () => {
         });
       });
   });
-  it('retreives an array of articles in descending date order', () => {
+  it('200: retrieves an array of articles in descending date order', () => {
     return request(app)
       .get('/api/articles')
       .expect(200)
@@ -49,8 +73,8 @@ describe('GET /', () => {
   });
 });
 
-describe('GET /:articleId', () => {
-  it('retreives a single article with the given articleId', () => {
+describe('GET /api/articles/:articleId', () => {
+  it('200: retrieves a single article with the given articleId', () => {
     return request(app)
       .get('/api/articles/1')
       .expect(200)
@@ -71,7 +95,7 @@ describe('GET /:articleId', () => {
         expect(typeof article_img_url).toBe('string');
       });
   });
-  it('throws a 404 error when given an articleId that does not exist', () => {
+  it('404: throws a 404 error when given an articleId that does not exist', () => {
     const articleId = 100000;
 
     return request(app)
@@ -90,7 +114,7 @@ describe('GET /:articleId', () => {
         expect(typeof path).toBe('string');
       });
   });
-  it('throws a 400 error when given an invalid articeId', () => {
+  it('400: throws a 400 error when given an invalid articeId', () => {
     return request(app)
       .get('/api/articles/invalid')
       .expect(400)
@@ -109,8 +133,8 @@ describe('GET /:articleId', () => {
   });
 });
 
-describe('GET /:articleId/comments', () => {
-  it('retreives a list of comments for the given articleId', () => {
+describe('GET /api/articles/:articleId/comments', () => {
+  it('200: retrieves a list of comments for the given articleId', () => {
     return request(app)
       .get('/api/articles/1/comments')
       .expect(200)
@@ -126,6 +150,50 @@ describe('GET /:articleId/comments', () => {
         // expect(typeof created_at).toBe('string');
         // expect(typeof votes).toBe('number');
         // expect(typeof article_img_url).toBe('string');
+      });
+  });
+});
+
+// USERS
+describe('GET /api/users', () => {
+  it('200: retrieves an array of users with the correct data', () => {
+    return request(app)
+      .get('/api/users')
+      .expect(200)
+      .then(res => {
+        const users = res.body.users;
+
+        expect(Array.isArray(users)).toBe(true);
+        expect(users.length).toBeGreaterThan(0);
+
+        users.forEach(user => {
+          const { username, name, avatar_url } = user;
+
+          expect(typeof username).toBe('string');
+          expect(typeof name).toBe('string');
+          expect(typeof avatar_url).toBe('string');
+        });
+      });
+  });
+});
+
+// 404
+describe('GET /[invalidPath]', () => {
+  it('returns a 404 error when visiting an invalid path', () => {
+    return request(app)
+      .get('/invalidPath')
+      .expect(404)
+      .then(res => {
+        const error = res.body.error;
+
+        expect(error).toBeInstanceOf(Object);
+
+        const { timestamp, status, errorMessage, path } = error;
+
+        expect(typeof timestamp).toBe('string');
+        expect(status).toBe(404);
+        expect(errorMessage).toBe('Not found');
+        expect(typeof path).toBe('string');
       });
   });
 });
