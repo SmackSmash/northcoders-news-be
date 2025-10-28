648a3cd37a0bf40f05eb4bb8438bddd9a349cf8d
Dan Moffat <danmoffat@gmail.com>
2025-10-28 11:30:15 +0000

    Fleshed out API docs a bit

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = express();
 
 app.use(express.json());
 
-app.use('/api', require('./routes/api/root'));
+app.use('/api', express.static('public'));
 app.use('/api/topics', require('./routes/api/topics'));
 app.use('/api/articles', require('./routes/api/articles'));
 app.use('/api/users', require('./routes/api/users'));
--- controllers/comments.js
+++ controllers/comments.js
@@ -1,17 +1,6 @@
 const { deleteCommentById, readCommentsByArticleId, createComment } = require('../models/comments');
 const { AppError } = require('./errors');
 
-// @route   DELETE /api/comments/:commentId
-// @desc    Delete comment by commentId
-exports.removeCommentById = async (req, res) => {
-  const { commentId } = req.params;
-
-  const deletedRowCount = await deleteCommentById(commentId);
-  if (deletedRowCount === 0) throw new AppError(`No comment found with id ${commentId}`, 404, req);
-
-  res.status(204).send({ comment: 'Comment deleted successfully' });
-};
-
 // @route   GET /api/articles/:articleId/comments
 // @desc    Get comments for article
 exports.getCommentsByArticleId = async (req, res) => {
@@ -34,4 +23,15 @@ exports.addComment = async (req, res) => {
   if (!addedComment) throw new AppError(`Comment was not added`, 400, req);
 
   res.status(200).send({ comment: addedComment });
+};
+
+// @route   DELETE /api/comments/:commentId
+// @desc    Delete comment by commentId
+exports.removeCommentById = async (req, res) => {
+  const { commentId } = req.params;
+
+  const deletedRowCount = await deleteCommentById(commentId);
+  if (deletedRowCount === 0) throw new AppError(`No comment found with id ${commentId}`, 404, req);
+
+  res.status(204).send({ comment: 'Comment deleted successfully' });
 };
