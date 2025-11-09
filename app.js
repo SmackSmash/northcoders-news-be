6551fb618e86bf178d989d9d6e8fc02e1e2c8326
Dan Moffat <danmoffat@gmail.com>
2025-11-09 00:58:30 +0000

    Added CORS

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const cors = require('cors');
 const { notFoundHandler, dbErrorHandler, appErrorHandler } = require('./controllers/errors');
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 app.use('/api', express.static('public'));
 app.use('/api/topics', require('./routes/api/topics'));
